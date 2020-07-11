a0a56ec88e5ab016b61a7efac8cdc68ef81bf6d5
pepelazz <pepelazz00@gmail.com>
2020-07-11 17:59:45 +0300

    Updates

---
+++ precache-manifest.fd4b2750a45944b3f2fcec01d41e4244.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a75a486ea98ee13e64663629c8ca87",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ec41b0945027ec37021",
+    "url": "./static/css/2.a6674286.chunk.css"
+  },
+  {
+    "revision": "19b38f605f02dc44690d",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "6ec41b0945027ec37021",
+    "url": "./static/js/2.2139a451.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2139a451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19b38f605f02dc44690d",
+    "url": "./static/js/main.a3f7919e.chunk.js"
+  },
+  {
+    "revision": "139c1ee812354f633930",
+    "url": "./static/js/runtime-main.7cde1131.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
